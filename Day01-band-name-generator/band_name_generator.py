801a6798a722600a14ff63f5f8d11b6cda8918cb
HarshvardhanAgrawal02 <historophile3100@gmail.com>
2026-02-10 11:43:17 +0530

    Day-01 complete

---
+++ Day01-band-name-generator/band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator")
+City_name = input("What's the name of the city you grew up in?\n")
+Pet_name = input("What's the name of your pet?\n")
+print("Your band name could be " + City_name + " " + Pet_name)
