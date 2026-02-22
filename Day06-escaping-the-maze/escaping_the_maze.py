16c21a50a322515944c0dc2f804a10702b501c09
HarshvardhanAgrawal02 <historophile3100@gmail.com>
2026-02-22 08:56:59 +0530

    update escaping maze problem code

---
+++ Day06-escaping-the-maze/escaping_the_maze.py
@@ -0,0 +1,19 @@
+#link of problem:- https://reeborg.ca/reeborg.html
+
+#code for the maze problem:-
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+            
+# while not at_goal():
+#     if wall_on_right() and not wall_in_front():
+#          move()
+#     elif right_is_clear():
+#          turn_right()
+#          move()
+#     elif wall_in_front() and not right_is_clear():
+#          turn_left()
+# here, instead of last elif i didnt think
+# of using else 
+ 
